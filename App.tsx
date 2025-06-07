73a244bc727ca0a66228ebec884ed690f35f5ffb
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-06-07 19:04:17 +0800

    refactor: call the AppNavigator

--- App.tsx
+++ App.tsx
@@ -1,25 +1,9 @@
-import { View, Text, TouchableOpacity } from "react-native";
+// App.tsx
 import React from "react";
-import { useNotification } from "./src/notifications/useNotification";
-import appStyles from "./src/styles/appStyles";
+import AppNavigator from "./src/navigation/AppNavigator";
 
-const App:React.FC = () => {
-
-  useNotification();
-
-  return (
-    <View style={appStyles.container}>
-      <Text style={appStyles.text}>RideAlert</Text>
-      <TouchableOpacity
-        onPress={() => {
-          console.log("Button Pressed");
-        }}
-        style={appStyles.pressContainer}
-      >
-        <Text style={appStyles.pressText}>Get Started</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
+const App: React.FC = () => {
+  return <AppNavigator />;
+};
 
 export default App;
