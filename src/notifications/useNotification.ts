ccba4f27ec10ac43a1e04a8afa3754d679450b27
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-06-07 22:08:21 +0800

    refactor: added a async to save the fcm token

--- src/notifications/useNotification.ts
+++ src/notifications/useNotification.ts
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import {PermissionsAndroid} from 'react-native';
 import messaging, { getMessaging } from '@react-native-firebase/messaging';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const requestUserPermission = async () => {
     const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
@@ -16,6 +17,10 @@ const getToken = async () => {
         const messaging = getMessaging();
         const token = await messaging.getToken();
         console.log('FCM Token:', token);
+        if (token) {
+            await AsyncStorage.setItem
+            ('fcm_token', token);
+        }
     } catch (error) {
         console.error('Error getting FCM token:', error);
     }
