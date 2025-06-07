164a1dc1f8c7a9f52206cb226780d6c0e119130e
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-06-07 22:07:58 +0800

    refactor: added login and home screen navigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -3,12 +3,19 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+// Tanan screens
 import InitialScreen from '../screens/Initial/InitialScreen';
+import LoginScreen from '../screens/Login/LoginScreen';
+import Home from '../screens/Dashboard/Home';
 
 export type RootStackParamList = {
   Initial: undefined;
+  Login: undefined;
+  Home: undefined;
   // You can add other screens here later, e.g. Home: undefined
 };
+
+//adb reverse tcp:8081 tcp:8081 to run it again
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -19,6 +26,8 @@ const AppNavigator: React.FC = () => {
         initialRouteName="Initial"
         screenOptions={{headerShown: false}}>
         <Stack.Screen name="Initial" component={InitialScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
