034ec06c5c6261e43e747ed7ca6ea7c3b4e8c013
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-08-21 23:28:21 +0800

    refactor: bottomnavbar remove

--- src/navigation/BottomNavigator.tsx
+++ src/navigation/BottomNavigator.tsx
@@ -1,36 +1,36 @@
-// navigation/BottomNavigator.tsx
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from '../screens/Dashboard/Home/Home';
-import Profile from '../screens/Dashboard/Profile/Profile';
-import Notification from '../screens/Dashboard/Notification/Notification';
-import Icon from 'react-native-vector-icons/Ionicons';
+// // navigation/BottomNavigator.tsx
+// import React from 'react';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import Home from '../screens/Dashboard/Home/Home';
+// import Profile from '../screens/Dashboard/Profile/Profile';
+// import Notification from '../screens/Dashboard/Notification/Notification';
+// import Icon from 'react-native-vector-icons/Ionicons';
 
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
 
-const BottomNavigator = () => {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        headerShown: false,
-        tabBarActiveTintColor: '#007AFF',
-        tabBarInactiveTintColor: 'gray',
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName = '';
+// const BottomNavigator = () => {
+//   return (
+//     <Tab.Navigator
+//       screenOptions={({ route }) => ({
+//         headerShown: false,
+//         tabBarActiveTintColor: '#007AFF',
+//         tabBarInactiveTintColor: 'gray',
+//         tabBarIcon: ({ focused, color, size }) => {
+//           let iconName = '';
 
-          if (route.name === 'Home') iconName = focused ? 'home' : 'home-outline';
-          else if (route.name === 'Profile') iconName = focused ? 'person' : 'person-outline';
-          else if (route.name === 'Notifications') iconName = focused ? 'notifications' : 'notifications-outline';
+//           if (route.name === 'Home') iconName = focused ? 'home' : 'home-outline';
+//           else if (route.name === 'Profile') iconName = focused ? 'person' : 'person-outline';
+//           else if (route.name === 'Notifications') iconName = focused ? 'notifications' : 'notifications-outline';
 
-          return <Icon name={iconName} size={size} color={color} />;
-        },
-      })}
-    >
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Notifications" component={Notification} />
-    </Tab.Navigator>
-  );
-};
+//           return <Icon name={iconName} size={size} color={color} />;
+//         },
+//       })}
+//     >
+//       <Tab.Screen name="Home" component={Home} />
+//       <Tab.Screen name="Profile" component={Profile} />
+//       <Tab.Screen name="Notifications" component={Notification} />
+//     </Tab.Navigator>
+//   );
+// };
 
-export default BottomNavigator;
+// export default BottomNavigator;
