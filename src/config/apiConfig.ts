48da1b13b9b454636e729e76f4cc707496e63a17
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-09-07 18:58:45 +0800

    refactor: change the BASE_URL to deployed

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://192.168.1.7:8000';
+export const BASE_URL = 'https://ridealert-backend.onrender.com';
 export const wsUrl = 'ws://192.168.1.7:8000/notifications/user';
 export const wsUrl_Available='ws://192.168.1.7:8000/ws/vehicles/available'
