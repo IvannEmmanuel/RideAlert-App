e0664441a95ce25d18341b596f69d1c692de7035
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-10-05 00:04:51 +0800

    refactor: making the apiConfig work

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,9 +1,9 @@
-// export const BASE_URL = 'https://ridealert-backend.onrender.com';
-// export const WS_BASE_URL = 'wss://ridealert-backend.onrender.com';
-// export const wsUrl = 'wss://ridealert-backend.onrender.com/notifications/user';
-// export const wsUrl_Available = 'wss://ridealert-backend.onrender.com/ws/vehicles/available';
+export const BASE_URL = 'https://ridealert-backend.onrender.com';
+export const WS_BASE_URL = 'wss://ridealert-backend.onrender.com';
+export const wsUrl = 'wss://ridealert-backend.onrender.com/notifications/user';
+export const wsUrl_Available = 'wss://ridealert-backend.onrender.com/ws/vehicles/available';
 
-export const BASE_URL = 'http://192.168.1.5:8000';
-export const WS_BASE_URL = 'ws://192.168.1.5:8000';
-export const wsUrl = 'ws://192.168.1.5:8000/notifications/user';
-export const wsUrl_Available = 'ws://192.168.1.5:8000/ws/vehicles/available';
+// export const BASE_URL = 'http://192.168.1.5:8000';
+// export const WS_BASE_URL = 'ws://192.168.1.5:8000';
+// export const wsUrl = 'ws://192.168.1.5:8000/notifications/user';
+// export const wsUrl_Available = 'ws://192.168.1.5:8000/ws/vehicles/available';
