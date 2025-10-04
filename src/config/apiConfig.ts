c304692c9bb21e7506949133872759c94386751d
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-10-04 15:39:49 +0800

    refactor: added some shits

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -4,6 +4,6 @@
 // export const wsUrl_Available = 'wss://ridealert-backend.onrender.com/ws/vehicles/available';
 
 export const BASE_URL = 'http://192.168.1.5:8000';
-export const WS_BASE_URL = 'wss://192.168.1.5:8000';
-export const wsUrl = 'wss://192.168.1.5:8000/notifications/user';
-export const wsUrl_Available = 'wss://192.168.1.5:8000/ws/vehicles/available';
+export const WS_BASE_URL = 'ws://192.168.1.5:8000';
+export const wsUrl = 'ws://192.168.1.5:8000/notifications/user';
+export const wsUrl_Available = 'ws://192.168.1.5:8000/ws/vehicles/available';
