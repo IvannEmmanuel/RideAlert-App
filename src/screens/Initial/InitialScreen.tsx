9edef0712628e5b70a39de1b8a06f3c6b42e8d47
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-10-09 00:01:00 +0800

    refactor: remove loading area

--- src/screens/Initial/InitialScreen.tsx
+++ src/screens/Initial/InitialScreen.tsx
@@ -19,67 +19,10 @@ const { height, width } = Dimensions.get('window');
 
 const InitialScreen = () => {
   const navigation = useNavigation();
-  const [loading, setLoading] = useState(true);
-
-  const checkSession = async () => {
-    try {
-      const refresh_token = await AsyncStorage.getItem("refresh_token");
-      const userData = await AsyncStorage.getItem("user");
-
-      if (refresh_token && userData) {
-        try {
-          // ✅ Try validating the current access token
-          const res = await axios.get(`${BASE_URL}/users/me`, {
-            headers: { Authorization: `Bearer ${refresh_token}` },
-          });
-
-          if (res.status === 200) {
-            console.log("✅ Session restored (refresh token valid)");
-            navigation.reset({ index: 0, routes: [{ name: "Home" }] });
-            return;
-          }
-        } catch (error) {
-          // ❌ Token might be expired
-          console.log("⚠️ Access token expired. Trying to refresh...");
-          const newToken = await refreshAccessToken();
-
-          if (newToken) {
-            console.log("🔄 Access token refreshed. Redirecting...");
-            navigation.reset({ index: 0, routes: [{ name: "Home" }] });
-            return;
-          }
-        }
-      }
-
-      // 🚪 If no tokens or refresh fails → go to Get Started screen
-      setLoading(false);
-    } catch (err) {
-      console.error("❌ Error checking session:", err);
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    checkSession();
-  }, []);
-
+  
   const getStartedPress = () => {
     navigation.navigate("InitialSecondPhase");
   };
-
-  if (loading) {
-    return (
-      <View style={{ justifyContent: "center", alignItems: 'center' }}>
-        <Image source={require('../../assets/Logo.png')} style={{ width: width * 0.8, height: height * 0.3, borderRadius: 20, top: height * 0.3 }} />
-        <LottieView
-          source={require('../../images/Loading.json')}
-          autoPlay
-          loop
-          style={{ width: width * 0.7, height: height * 0.7 }}
-        />
-      </View>
-    );
-  }
 
   return (
     <ImageBackground
