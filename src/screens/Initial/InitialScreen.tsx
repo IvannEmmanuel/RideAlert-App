921fc3ee798b66e58ce48347b3b362a9701a28e3
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-10-08 21:34:54 +0800

    refactor: added a check session for refresh_token and lottieview added for loading

--- src/screens/Initial/InitialScreen.tsx
+++ src/screens/Initial/InitialScreen.tsx
@@ -5,7 +5,7 @@ import {
   TouchableOpacity,
   View,
   ImageBackground,
-  ActivityIndicator,
+  Dimensions
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -13,6 +13,9 @@ import axios from "axios";
 import initialScreenStyles from "../../styles/initialScreenStyles";
 import { BASE_URL } from "../../config/apiConfig";
 import { refreshAccessToken } from "../../utils/authStorage";
+import LottieView from "lottie-react-native";
+
+const { height, width } = Dimensions.get('window');
 
 const InitialScreen = () => {
   const navigation = useNavigation();
@@ -20,18 +23,18 @@ const InitialScreen = () => {
 
   const checkSession = async () => {
     try {
-      const accessToken = await AsyncStorage.getItem("access_token");
+      const refresh_token = await AsyncStorage.getItem("refresh_token");
       const userData = await AsyncStorage.getItem("user");
 
-      if (accessToken && userData) {
+      if (refresh_token && userData) {
         try {
           // ✅ Try validating the current access token
           const res = await axios.get(`${BASE_URL}/users/me`, {
-            headers: { Authorization: `Bearer ${accessToken}` },
+            headers: { Authorization: `Bearer ${refresh_token}` },
           });
 
           if (res.status === 200) {
-            console.log("✅ Session restored (access token valid)");
+            console.log("✅ Session restored (refresh token valid)");
             navigation.reset({ index: 0, routes: [{ name: "Home" }] });
             return;
           }
@@ -66,9 +69,14 @@ const InitialScreen = () => {
 
   if (loading) {
     return (
-      <View style={[initialScreenStyles.container, { justifyContent: "center" }]}>
-        <ActivityIndicator size="large" color="#007AFF" />
-        <Text style={{ color: "#007AFF", marginTop: 10 }}>Checking session...</Text>
+      <View style={{ justifyContent: "center", alignItems: 'center' }}>
+        <Image source={require('../../assets/Logo.png')} style={{ width: width * 0.8, height: height * 0.3, borderRadius: 20, top: height * 0.3 }} />
+        <LottieView
+          source={require('../../images/Loading.json')}
+          autoPlay
+          loop
+          style={{ width: width * 0.7, height: height * 0.7 }}
+        />
       </View>
     );
   }
